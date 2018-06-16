97bd5c55bf881115a3b2ff10981a41d62cc9bb8c
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-16 15:40:42 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,6 +3,7 @@
 def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
+    number -=
   end
   "HAPPY NEW YEAR!"
 end
@@ -11,6 +12,7 @@ def countdown_with_sleep(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     sleep 1
+    number -=
   end
   "HAPPY NEW YEAR!"
 end
